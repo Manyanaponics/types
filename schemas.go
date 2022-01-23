acce173466f335bcf8f8902d8970104df68bb145
Jack Mcgregor <jackjwmcgregor@gmail.com>
2022-01-22 19:16:38 -0500

    updated fields

--- schemas.go
+++ schemas.go
@@ -37,22 +37,31 @@ type CulinaryFruit int64
 
 // Pomes - any crunchy accessory fruit that surrounds the fruit's inedible "core" (composed of the plant's endocarp) and typically has its seeds arranged in a star-like pattern
 // // apples / pears
+
 // Drupes - any fruit that has only one seed (or "stone") or one hard capsule containing seeds.
 // // cherry / coconut / mango
+
 // Botanical_berries - any fruit that has a relatively thin exterior, with mostly flesh and more than one seed inside
 // // blueberry / cranberry / red currant
+
 // Pepos - any fruit that is covered by a hard, thick rind with soft flesh inside, and seeds filling each locule. Melons are good examples of this.
 // // melons
+
 // Hesperidiums - Also known as citruses, they possess thick and leathery rinds. These fruits are generally sour and acidic to some extent and have a wagon wheel-like cross section
 // // lemons / limes / oranges
+
 // Aggregate_fruits - a cluster of many fruits produced from a single flower
 // // raspberry / blackberry
+
 // Multiple fruits - a cluster of many fruits produced from multiple flowers
 // // fig / pineapple / jackfruit
+
 // Capsules - a pod fruit with multiple carpels
 // // a pod fruit with one carpel
+
 // Legumes - a pod fruit with one carpel
 // // Ice-cream bean
+
 // Folicies - a single ovary that splits along a single seam
 // // Dead man's fingers
 
@@ -95,4 +104,16 @@ type Crop struct {
 type Taxonomy struct {
 	Genus  string
 	Family string
+}
+
+type Category struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
+type WikiCrop struct {
+	Category      Category `json:"category"`
+	Common_name   string   `json:"common_name"`
+	Species_name  string   `json:"species_name"`
+	Cultivar_list string   `json:"cultivar_list"`
 }
