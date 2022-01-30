90168404eb72415be4e4c30a95b953cb31371651
Jack Mcgregor <jackjwmcgregor@gmail.com>
2022-01-29 19:28:28 -0500

    updated new master type

--- schemas.go
+++ schemas.go
@@ -1,81 +1,9 @@
-package schemas
+package types
 
 import (
 	"time"
 
 	"github.com/gofrs/uuid"
-)
-
-type CropPlantType int64
-
-const (
-	Tree   CropPlantType = 0
-	Vines  CropPlantType = 1 // tomatoes / cucumbers
-	Shrubs CropPlantType = 2 // berries
-	Spices CropPlantType = 3 // bark / pods
-	Herbs  CropPlantType = 4 // leaves / stem
-	Plant  CropPlantType = 5 // aubergine
-)
-
-type CropSource int64
-
-// wikipedia
-const (
-	Culinary_fruits           CropSource = 0
-	Culinary_herbs_and_spices CropSource = 1
-	Culinary_nuts             CropSource = 2
-	Edible_cacti              CropSource = 3
-	Edible_flowers            CropSource = 4
-	Edible_seeds              CropSource = 5
-	Forageable_plants         CropSource = 6
-	Leaf_vegetables           CropSource = 7
-	Root_vegetables           CropSource = 8
-	Vegetables                CropSource = 9
-)
-
-type CulinaryFruit int64
-
-// Pomes - any crunchy accessory fruit that surrounds the fruit's inedible "core" (composed of the plant's endocarp) and typically has its seeds arranged in a star-like pattern
-// // apples / pears
-
-// Drupes - any fruit that has only one seed (or "stone") or one hard capsule containing seeds.
-// // cherry / coconut / mango
-
-// Botanical_berries - any fruit that has a relatively thin exterior, with mostly flesh and more than one seed inside
-// // blueberry / cranberry / red currant
-
-// Pepos - any fruit that is covered by a hard, thick rind with soft flesh inside, and seeds filling each locule. Melons are good examples of this.
-// // melons
-
-// Hesperidiums - Also known as citruses, they possess thick and leathery rinds. These fruits are generally sour and acidic to some extent and have a wagon wheel-like cross section
-// // lemons / limes / oranges
-
-// Aggregate_fruits - a cluster of many fruits produced from a single flower
-// // raspberry / blackberry
-
-// Multiple fruits - a cluster of many fruits produced from multiple flowers
-// // fig / pineapple / jackfruit
-
-// Capsules - a pod fruit with multiple carpels
-// // a pod fruit with one carpel
-
-// Legumes - a pod fruit with one carpel
-// // Ice-cream bean
-
-// Folicies - a single ovary that splits along a single seam
-// // Dead man's fingers
-
-const (
-	Pomes             CulinaryFruit = 1
-	Drupes            CulinaryFruit = 2
-	Botanical_berries CulinaryFruit = 3
-	Pepos             CulinaryFruit = 4
-	Hesperidiums      CulinaryFruit = 5
-	Aggregate_fruits  CulinaryFruit = 6
-	Multiple_fruits   CulinaryFruit = 7
-	Capsules          CulinaryFruit = 8
-	Legumes           CulinaryFruit = 9
-	Follicles         CulinaryFruit = 10
 )
 
 type Crop struct {
@@ -99,11 +27,6 @@ type Crop struct {
 
 	// Taxonomy        Taxonomy      `gorm:"type:text[]"`
 	// Crop_type       CropPlantType `gorm:"type:text[]"`
-}
-
-type Taxonomy struct {
-	Genus  string
-	Family string
 }
 
 type Category struct {
---
+++ types.go
@@ -0,0 +1,212 @@
+package types
+
+// ************************************************************************
+// ***** BASIC *****
+// ************************************************************************
+
+// synonym / english / dutch / spanish / italian
+
+type OtherNames struct {
+	Lang string
+	Name string
+}
+
+type Recipes struct {
+	Name        string
+	Description string
+}
+
+type Taxonomy struct {
+	Kingdom string
+	Phylum  string
+	Class   string
+	Order   string
+	Family  string
+	Genus   string
+	Species string
+}
+
+type Season int64
+
+const (
+	Summer Season = iota
+	Autumn
+	Winter
+	Spring
+)
+
+type CropPlantType int64
+
+const (
+	Tree   CropPlantType = 0
+	Vines  CropPlantType = 1 // tomatoes / cucumbers
+	Shrubs CropPlantType = 2 // berries
+	Spices CropPlantType = 3 // bark / pods
+	Herbs  CropPlantType = 4 // leaves / stem
+	Plant  CropPlantType = 5 // aubergine
+)
+
+type CropSource int64
+
+// wikipedia
+const (
+	Culinary_fruits           CropSource = 0
+	Culinary_herbs_and_spices CropSource = 1
+	Culinary_nuts             CropSource = 2
+	Edible_cacti              CropSource = 3
+	Edible_flowers            CropSource = 4
+	Edible_seeds              CropSource = 5
+	Forageable_plants         CropSource = 6
+	Leaf_vegetables           CropSource = 7
+	Root_vegetables           CropSource = 8
+	Vegetables                CropSource = 9
+)
+
+// belongs in SubCategory when more are documented
+type CulinaryFruit int64
+
+const (
+	Pomes             CulinaryFruit = 1
+	Drupes            CulinaryFruit = 2
+	Botanical_berries CulinaryFruit = 3
+	Pepos             CulinaryFruit = 4
+	Hesperidiums      CulinaryFruit = 5
+	Aggregate_fruits  CulinaryFruit = 6
+	Multiple_fruits   CulinaryFruit = 7
+	Capsules          CulinaryFruit = 8
+	Legumes           CulinaryFruit = 9
+	Follicles         CulinaryFruit = 10
+)
+
+type BasicInfo struct {
+	Name           string
+	ScientificName string
+	Category       string   // fruit / vegetables / herb
+	SubCategory    string   // leafy_and_salad_vegetables / pomes
+	Notes          string   // Array<{name: string; description: string}>
+	Popularity     int64    // score out of 100; refers to host country
+	Varieties      []string // eg for onions, show 'red onion, spring onion, green onion, brown onion etc'
+	OtherNames     []OtherNames
+	Lifespan       Season
+	Taxonomy       Taxonomy
+	Recipes        Recipes
+	CropPlantType  CropPlantType
+	CropSource     CropSource
+}
+
+// ************************************************************************
+// ***** PLANTING *****
+// ************************************************************************
+
+type GrowMedium int64
+
+const (
+	CocoCoir = iota
+	ExpandedClayPellets
+	Pebbles
+	Gravel
+	HempFiber
+	Perlite
+	PhenolicFoam
+	Rockwool
+	Sand
+)
+
+type HydroponicType int64
+
+const (
+	WickSystems = iota
+	DeepWaterCulture
+	NutrientFilmTechnique
+	EbbAndFlow
+	AeroponicsTowerPressurized
+	AeroponicsTowerTrickle
+	AeroponicsTub
+	DripSystems
+)
+
+type Planting struct {
+	SpacingOptimumCM int64
+	NetPotSizeCM     int64
+	DaysGerminating  int64
+	HydroponicType   []HydroponicType
+	GrowingMedium    []GrowMedium
+}
+
+// ************************************************************************
+// ***** HARVESTING *****
+// ************************************************************************
+
+type HarvestableParts int64
+
+const (
+	Leaf = iota
+	Flower
+	Stalk
+	Root
+	Bulb
+)
+
+type Harvesting struct {
+	DaysToHarvestEarliestNonHydro int64
+	DaysToHarvestEarliestHydro    int64
+	DaysToHarvestNonHydro         int64
+	DaysToHarvestHydro            int64
+	HarvestCostNonHydro           int64
+	HarvestCostHydro              int64
+	AverageCropPriceRetail        int64
+	AverageCropPriceWholesale     int64
+	HarvestableParts              []HarvestableParts
+}
+
+// ************************************************************************
+// ***** DIMENSIONS *****
+// ************************************************************************
+
+// all values are average
+type Dimensions struct {
+	HarvestWeightNonHydro   int64
+	HarvestWeightHydro      int64
+	SquareMetersPerUnit     int64
+	WaterTemperatureAtRoots int64
+	Height                  int64
+	MaximumWidth            int64
+}
+
+// ************************************************************************
+// ***** ENVIRONMENT *****
+// ************************************************************************
+
+type Environment struct {
+	GrowthZones           int64 // 1-12
+	TempMin               float64
+	TempMax               float64
+	TempOptimum           float64
+	RootTempMin           float64 // For most case this will default to temp_optimum, but roots can be sensitive
+	RootTempMax           float64
+	RootTempOptimum       float64
+	OxygenMin             float64
+	OxygenOptimum         float64
+	Co2Min                float64
+	Co2Optimum            float64
+	HumidityMin           float64
+	HumidityMax           float64
+	PHMin                 float64
+	PHMax                 float64
+	PHOptimum             float64
+	LightHoursMin         int64
+	LightHoursMax         int64
+	LightHoursOptimum     int64
+	LightSpectrumNmMin    int64
+	LightSpectrumNmMax    int64
+	LightSpectrumNeedsPar bool
+	LightSpectrumOther    string // eg UV(B) light can enhance production of certain oils
+}
+
+type CropMaster struct {
+	BasicInfo   BasicInfo
+	Planting    Planting
+	Harvesting  Harvesting
+	Dimensions  Dimensions
+	Environment Environment
+}
